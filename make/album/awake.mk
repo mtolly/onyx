6bf1d1d58dbe8f657aa3f13488ad7727e67b327b
Michael Tolly <onyxite@gmail.com>
2014-03-08 12:32:16 -0600

    started work for removing all the songs.dta files

---
+++ make/album/awake.mk
@@ -0,0 +1,5 @@
+album = Awake
+year = 1994
+genre = metal
+subgenre = subgenre_prog
+gender = male
---
+++ make/dta-drums.mk
@@ -0,0 +1,22 @@
+preview-start ?= 0
+preview-end ?= `echo "$(preview-start) + 30000" | bc`
+rank-drum ?= 1
+rank-band ?= 1
+
+gen/%/songs.dta: ../../dta/drums.dta gen/%/notes.mid
+	cat $< \
+		| sed "s/<TITLE>/$(title)/g" \
+		| sed "s/<ARTIST>/$(artist)/g" \
+		| sed "s/<PACKAGE>/$(package)/g" \
+		| sed "s/<LENGTH>/`../../scripts/song-length $(word 2,$+)`/g" \
+		| sed "s/<PREVIEW_START>/$(preview-start)/g" \
+		| sed "s/<PREVIEW_END>/$(preview-end)/g" \
+		| sed "s/<RANK_DRUM>/$(rank-drum)/g" \
+		| sed "s/<RANK_BAND>/$(rank-band)/g" \
+		| sed "s/<GENRE>/$(genre)/g" \
+		| sed "s/<GENDER>/$(gender)/g" \
+		| sed "s/<YEAR>/$(year)/g" \
+		| sed "s/<SUBGENRE>/$(subgenre)/g" \
+		| sed "s/<ALBUM>/$(album)/g" \
+		| sed "s/<NUMBER>/$(number)/g" \
+		> $@
