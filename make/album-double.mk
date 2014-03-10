8af361b9797b5ba0f0a87615029cdc2db93c66d5
Michael Tolly <onyxite@gmail.com>
2014-03-10 15:44:46 -0500

    fadeout and two-tracks for album audio

--- make/album-double.mk
+++ make/album-double.mk
@@ -4,10 +4,19 @@ album-fadein  ?= 0
 album-pad     ?= 0
 album-cutoff  ?= 0
 album-fadeout ?= 0
+next-cutoff   ?= 0
 
-gen/album/%/song-untimed.wav: audio-album.*
+gen/album/%/this.wav: audio-this.*
 	mkdir -p $(@D)
 	../../scripts/audioconvert $< $@ rate 44100 channels 2
+
+gen/album/%/next.wav: audio-next.*
+	mkdir -p $(@D)
+	../../scripts/audioconvert $< $@ rate 44100 channels 2 trim 0 $(next-cutoff)
+
+gen/album/%/song-untimed.wav: gen/album/%/this.wav gen/album/%/next.wav
+	mkdir -p $(@D)
+	sox --combine concatenate $+ $@
 
 gen/album/%.wav: gen/album/%-untimed.wav
 	sox $< $@ \
