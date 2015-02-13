f3ff265c5da2df29dc241cd2f538b918921b9f9e
Michael Tolly <miketolly@gmail.com>
2015-02-13 17:38:06 -0600

    move the artist folders into their own dir
    closes #35

--- build/src/Main.hs
+++ build/src/Main.hs
@@ -149,7 +149,7 @@ countinRules :: Song -> Rules ()
 countinRules s = eachVersion s $ \_ dir -> do
   dir </> "countin.wav" *> \out -> do
     let mid = dir </> "notes.mid"
-        hit = "../../sound/hihat-foot.wav"
+        hit = "../../../sound/hihat-foot.wav"
     makeCountin mid hit out
   dir </> "song-countin.wav" *> \out -> do
     let song = File $ dir </> "song.wav"
