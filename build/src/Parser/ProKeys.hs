1cb579a97e0340485fd289f5aefa6a51ccda22d0
Michael Tolly <miketolly@gmail.com>
2015-05-22 19:23:18 -0500

    use better type for pro keys notes
    [ci skip]

--- build/src/Parser/ProKeys.hs
+++ build/src/Parser/ProKeys.hs
@@ -21,13 +21,16 @@ data Event
   | Trill Bool -- ^ Fill lanes on two keys.
   | Overdrive Bool -- ^ An energy phrase.
   | BRE Bool -- ^ Fill lanes for a Big Rock Ending.
-  | Note Int Bool -- ^ Valid pitches are in MIDI range 48 to 72.
+  | Note ProKey Bool
   deriving (Eq, Ord, Show)
 
 -- | There are six playable ranges, each of which covers 10 white keys, plus
 -- all the black keys within. They are named here according to their lowest key.
 data LaneRange = RangeC | RangeD | RangeE | RangeF | RangeG | RangeA
   deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
+data ProKey = RedYellow Key | BlueGreen Key | OrangeC
+  deriving (Eq, Ord, Show, Read)
 
 instanceMIDIEvent [t| Event |]
 
@@ -37,12 +40,33 @@ instanceMIDIEvent [t| Event |]
   , blip 5 [p| LaneShift RangeF |]
   , blip 7 [p| LaneShift RangeG |]
   , blip 9 [p| LaneShift RangeA |]
-  , ( [e| firstEventWhich $ \e -> case isNoteEdge e of
-        Just (i, b) | 48 <= i && i <= 72 -> Just $ Note i b
-        _ -> Nothing
-      |]
-    , [e| \case Note i b -> RTB.singleton NNC.zero $ makeEdge i b |]
-    )
+
+  , edge 48 $ applyB [p| Note (RedYellow C ) |]
+  , edge 49 $ applyB [p| Note (RedYellow Cs) |]
+  , edge 50 $ applyB [p| Note (RedYellow D ) |]
+  , edge 51 $ applyB [p| Note (RedYellow Ds) |]
+  , edge 52 $ applyB [p| Note (RedYellow E ) |]
+  , edge 53 $ applyB [p| Note (RedYellow F ) |]
+  , edge 54 $ applyB [p| Note (RedYellow Fs) |]
+  , edge 55 $ applyB [p| Note (RedYellow G ) |]
+  , edge 56 $ applyB [p| Note (RedYellow Gs) |]
+  , edge 57 $ applyB [p| Note (RedYellow A ) |]
+  , edge 58 $ applyB [p| Note (RedYellow As) |]
+  , edge 59 $ applyB [p| Note (RedYellow B ) |]
+  , edge 60 $ applyB [p| Note (BlueGreen C ) |]
+  , edge 61 $ applyB [p| Note (BlueGreen Cs) |]
+  , edge 62 $ applyB [p| Note (BlueGreen D ) |]
+  , edge 63 $ applyB [p| Note (BlueGreen Ds) |]
+  , edge 64 $ applyB [p| Note (BlueGreen E ) |]
+  , edge 65 $ applyB [p| Note (BlueGreen F ) |]
+  , edge 66 $ applyB [p| Note (BlueGreen Fs) |]
+  , edge 67 $ applyB [p| Note (BlueGreen G ) |]
+  , edge 68 $ applyB [p| Note (BlueGreen Gs) |]
+  , edge 69 $ applyB [p| Note (BlueGreen A ) |]
+  , edge 70 $ applyB [p| Note (BlueGreen As) |]
+  , edge 71 $ applyB [p| Note (BlueGreen B ) |]
+  , edge 72 $ applyB [p| Note OrangeC        |]
+
   , edge 115 $ applyB [p| Solo |]
   , edge 116 $ applyB [p| Overdrive |]
   , edge 120 $ applyB [p| BRE |]
