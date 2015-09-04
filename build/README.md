ce29f2f5e12ef0143ce5412a4dddde5bad2e77f0
Michael Tolly <miketolly@gmail.com>
2015-09-03 23:04:11 -0500

    working on windows!

---
+++ build/README.md
@@ -0,0 +1,22 @@
+## Requirements
+
+* `stack`
+
+* On Windows:
+
+  * .NET Framework v3.5
+  * Visual C++ 2008 Runtime
+  * MSYS2
+  * `make` (`pacman -S make`)
+
+* On Mac:
+
+  * Mono
+  * Wine
+  * Homebrew
+
+## Build
+
+* On Windows: `make win-deps`, `make win`
+
+* On Mac: `make mac-deps`, `make mac`
--- build/src/Magma.hs
+++ build/src/Magma.hs
@@ -41,7 +41,7 @@ runMagmaMIDI proj mid = withSystemTempDirectory "magma" $ \tmp -> do
       mid'  = wd </> mid
   liftIO $ Dir.createDirectory $ tmp </> "gen"
   liftIO $ forM_ magmaFiles $ \(path, bs) -> B.writeFile (tmp </> path) bs
-  withExe (callProcessIn tmp) "MagmaCompilerC3.exe" ["-export_midi", proj', mid']
+  withExe (callProcessIn tmp) (tmp </> "MagmaCompilerC3.exe") ["-export_midi", proj', mid']
 
 runMagma :: FilePath -> FilePath -> Action ()
 runMagma proj rba = withSystemTempDirectory "magma" $ \tmp -> do
@@ -50,7 +50,7 @@ runMagma proj rba = withSystemTempDirectory "magma" $ \tmp -> do
       rba'  = wd </> rba
   liftIO $ Dir.createDirectory $ tmp </> "gen"
   liftIO $ forM_ magmaFiles $ \(path, bs) -> B.writeFile (tmp </> path) bs
-  withExe (callProcessIn tmp) "MagmaCompilerC3.exe" [proj', rba']
+  withExe (callProcessIn tmp) (tmp </> "MagmaCompilerC3.exe") [proj', rba']
 
 -- | Like the one from 'System.IO.Temp', but in the 'Action' monad.
 withSystemTempDirectory :: String -> (FilePath -> Action a) -> Action a
@@ -76,7 +76,7 @@ oggToMogg ogg mogg = withSystemTempDirectory "ogg2mogg" $ \tmp -> do
     $ sinkSnd (tmp </> "silence.wav")
     (Snd.Format Snd.HeaderFormatWav Snd.SampleFormatPcm16 Snd.EndianFile)
     (silent (Seconds 31) 44100 2 :: AudioSource (ResourceT IO) Int16)
-  _ <- withExe (callProcessIn tmp) "MagmaCompilerC3.exe" [proj, rba]
+  _ <- withExe (callProcessIn tmp) (tmp </> "MagmaCompilerC3.exe") [proj, rba]
   liftIO $ IO.withBinaryFile (tmp </> rba) IO.ReadMode $ \hrba -> do
     IO.hSeek hrba IO.AbsoluteSeek $ 4 + (4 * 3)
     moggOffset <- hReadWord32le hrba
