62f62360934c7b37afd46ed243d660f7b0937273
Michael Tolly <miketolly@gmail.com>
2015-12-09 19:19:38 -0600

    Preparing to work on that toe song again

--- haskell/build/src/Scripts.hs
+++ haskell/build/src/Scripts.hs
@@ -95,7 +95,7 @@ previewBounds mid = do
 songLength' :: Song U.Beats -> U.Beats
 songLength' s = case RTB.getTimes $ RTB.filter (== Events.End) $ allEvents s of
   [bts] -> bts
-  results -> error $ "songLength': error, " ++ show (length results) ++ " [end] events found"
+  _     -> 0 -- eh
 
 -- | Returns the time of the [end] event in milliseconds.
 songLength :: FilePath -> Action Int
