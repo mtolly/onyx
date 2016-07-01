26114ab0889d5618796589428065cedf31f7e2dd
Michael Tolly <miketolly@gmail.com>
2016-06-30 20:27:08 -0500

    Sevish progress; Reaper basic keys scripts

---
+++ scripts/reaper/setblue.py
@@ -0,0 +1,9 @@
+take = RPR_MIDIEditor_GetTake(RPR_MIDIEditor_GetActive())
+noteCount = RPR_MIDI_CountEvts(take, 0, 0, 0)[2]
+for i in range(noteCount):
+  note = RPR_MIDI_GetNote(take, i, 0, 0, 0, 0, 0, 0, 0)
+  selected = note[3]
+  pitch = note[8]
+  if selected:
+    RPR_MIDI_SetNote(take, i, -1, 0, -1, -1, -1, 99, -1, -1)
+RPR_MIDI_Sort(take)
