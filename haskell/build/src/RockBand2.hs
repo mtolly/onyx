ebc79f44443cc9db3d70d47a6787d696f098fe9a
Michael Tolly <miketolly@gmail.com>
2016-07-10 00:15:48 -0500

    RB2: fix missing grybo colors, fix [end] VENUE

--- haskell/build/src/RockBand2.hs
+++ haskell/build/src/RockBand2.hs
@@ -1,7 +1,7 @@
 module RockBand2 (convertMIDI, dryVoxAudio) where
 
 import qualified RockBand.File as F
-import Data.Maybe (mapMaybe)
+import Data.Maybe (mapMaybe, listToMaybe)
 import qualified Data.Set as Set
 import qualified Data.EventList.Absolute.TimeBody as ATB
 import qualified Data.EventList.Relative.TimeBody as RTB
@@ -13,12 +13,14 @@ import qualified RockBand.Venue as V
 import qualified Sound.MIDI.Util as U
 import Data.Conduit.Audio (AudioSource, Duration(Seconds), silent, concatenate, sine)
 import Scripts (trackGlue)
-import Data.List (partition, sort, nub)
+import Data.List (partition, sort, nub, inits, tails)
 import Control.Monad (guard)
 import Config (KeysRB2(..), Instrument(..))
 import qualified Sound.MIDI.File.Event as E
 import RockBand.Parse (unparseBlip, unparseOne, unparseCommand)
 import Data.Either (lefts, rights)
+import RockBand.Common (LongNote(..), Difficulty(..), joinEdges, splitEdges)
+import Data.Foldable (toList)
 
 dryVoxAudio :: (Monad m) => F.Song U.Beats -> AudioSource m Float
 dryVoxAudio f = let
@@ -40,7 +42,7 @@ dryVoxAudio f = let
 convertMIDI :: KeysRB2 -> U.Beats -> F.Song U.Beats -> F.Song U.Beats
 convertMIDI keysrb2 hopoThresh mid = mid
   { F.s_tracks = fixUnisons $ flip mapMaybe (F.s_tracks mid) $ \case
-    F.PartDrums  t -> Just $ F.PartDrums $ flip RTB.mapMaybe t $ \case
+    F.PartDrums  t -> Just $ F.PartDrums $ fixDrumColors $ flip RTB.mapMaybe t $ \case
       -- Drums.ProType{} -> Nothing -- Magma is fine with pro markers
       Drums.SingleRoll{} -> Nothing
       Drums.DoubleRoll{} -> Nothing
@@ -56,14 +58,14 @@ convertMIDI keysrb2 hopoThresh mid = mid
       x -> Just x
     F.PartGuitar t -> case keysrb2 of
       KeysGuitar -> Nothing
-      _          -> Just $ F.PartGuitar $ fixGB True  t
+      _          -> Just $ F.PartGuitar $ fixFiveColors $ fixGB True  t
     F.PartBass   t -> case keysrb2 of
       KeysBass -> Nothing
-      _        -> Just $ F.PartBass $ fixGB False t
+      _        -> Just $ F.PartBass $ fixFiveColors $ fixGB False t
     F.PartKeys   t -> case keysrb2 of
       NoKeys     -> Nothing
-      KeysGuitar -> Just $ F.PartGuitar $ fixGB True  $ Five.keysToGuitar hopoThresh t
-      KeysBass   -> Just $ F.PartBass   $ fixGB False $ Five.keysToGuitar hopoThresh t
+      KeysGuitar -> Just $ F.PartGuitar $ fixFiveColors $ fixGB True  $ Five.keysToGuitar hopoThresh t
+      KeysBass   -> Just $ F.PartBass   $ fixFiveColors $ fixGB False $ Five.keysToGuitar hopoThresh t
     F.PartVocals t -> Just $ F.PartVocals $ flip RTB.mapMaybe t $ \case
       Vox.LyricShift -> Nothing
       Vox.RangeShift{} -> Nothing
@@ -72,9 +74,13 @@ convertMIDI keysrb2 hopoThresh mid = mid
       Events.PracticeSection _ -> Nothing
       e -> Just e
     F.Beat  t -> Just $ F.Beat t
-    F.Venue v -> Just $ F.RawTrack $ convertVenue v
+    F.Venue v -> Just $ F.RawTrack $ convertVenue endPosn v
     _ -> Nothing
   } where
+    endPosn :: Maybe U.Beats
+    endPosn = listToMaybe $ do
+      F.Events t <- F.s_tracks mid
+      toList $ fmap (fst . fst) $ RTB.viewL $ RTB.filter (== Events.End) t
     fixGB hasSolos t = flip RTB.mapMaybe t $ \case
       Five.Tremolo{} -> Nothing
       Five.Trill{} -> Nothing
@@ -114,15 +120,26 @@ convertMIDI keysrb2 hopoThresh mid = mid
         then trks
         else map fixTrack trks
 
-convertVenue :: RTB.T U.Beats V.Event -> RTB.T U.Beats E.T
-convertVenue rtb = let
+convertVenue :: Maybe U.Beats -> RTB.T U.Beats V.Event -> RTB.T U.Beats E.T
+convertVenue endPosn rtb = let
   (z, nz) = U.trackSplitZero rtb
   z' = V.Lighting V.Lighting_verse : filter (\case V.Lighting _ -> False; _ -> True) z
-  in U.setTrackName "VENUE" $ RTB.flatten $ fmap nub $ RTB.collectCoincident
-    $ U.trackJoin $ fmap convertVenueInstant $ RTB.collectCoincident $ U.trackGlueZero z' nz
+  annotateEnding trk = case endPosn of
+    Nothing  -> annotate True trk
+    Just end -> case U.trackSplit (end - 1) trk of
+      (a, b) -> trackGlue (end - 1) (annotate True a) (annotate False b)
+  annotate b = fmap $ \x -> (b, x)
+  in U.setTrackName "VENUE"
+    $ RTB.flatten $ fmap nub $ RTB.collectCoincident
+    $ U.trackJoin
+    $ fmap (uncurry convertVenueInstant)
+    $ annotateEnding
+    $ RTB.collectCoincident
+    $ U.trackGlueZero z' nz
 
-convertVenueInstant :: [V.Event] -> RTB.T U.Beats E.T
-convertVenueInstant evts = let
+convertVenueInstant :: Bool -> [V.Event] -> RTB.T U.Beats E.T
+convertVenueInstant canMakeBlips evts = let
+  unparseBlip' p = if canMakeBlips then unparseBlip p else RTB.empty
   changed = flip mapMaybe evts $ \e -> case e of
     V.Camera c -> Just $ Left c
     V.SingalongGuitarKeys{} -> unchanged e
@@ -135,43 +152,43 @@ convertVenueInstant evts = let
     V.SpotlightDrums{} -> unchanged e
     V.PostProcess pp -> Just $ Right $ postproc pp
     V.Lighting l -> Just $ Right $ lighting l
-    V.LightingFirst -> Just $ Right $ unparseBlip 50
-    V.LightingPrev -> Just $ Right $ unparseBlip 49
-    V.LightingNext -> Just $ Right $ unparseBlip 48
+    V.LightingFirst -> Just $ Right $ unparseBlip' 50
+    V.LightingPrev -> Just $ Right $ unparseBlip' 49
+    V.LightingNext -> Just $ Right $ unparseBlip' 48
     V.BonusFX{} -> unchanged e
     V.BonusFXOptional{} -> unchanged e
   unchanged = Just . Right . unparseOne
   postproc = \case
-    V.PP_ProFilm_a -> unparseBlip 96
-    V.PP_ProFilm_b -> unparseBlip 96
-    V.PP_video_a -> unparseBlip 107
-    V.PP_film_16mm -> unparseBlip 98
-    V.PP_shitty_tv -> unparseBlip 109
-    V.PP_bloom -> unparseBlip 103
-    V.PP_film_sepia_ink -> unparseBlip 99
-    V.PP_film_silvertone -> unparseBlip 100
-    V.PP_film_b_w -> unparseBlip 108
-    V.PP_video_bw -> unparseBlip 108
-    V.PP_contrast_a -> unparseBlip 97
-    V.PP_photocopy -> unparseBlip 102
-    V.PP_film_blue_filter -> unparseBlip 106
-    V.PP_desat_blue -> unparseBlip 106
-    V.PP_video_security -> unparseBlip 109
-    V.PP_bright -> unparseBlip 104
-    V.PP_posterize -> unparseBlip 103
-    V.PP_clean_trails -> unparseBlip 110
-    V.PP_video_trails -> unparseBlip 110
-    V.PP_flicker_trails -> unparseBlip 110
-    V.PP_desat_posterize_trails -> unparseBlip 98
-    V.PP_film_contrast -> unparseBlip 97
-    V.PP_film_contrast_blue -> unparseBlip 106
-    V.PP_film_contrast_green -> unparseBlip 97
-    V.PP_film_contrast_red -> unparseBlip 97
-    V.PP_horror_movie_special -> unparseBlip 101
-    V.PP_photo_negative -> unparseBlip 101
-    V.PP_ProFilm_mirror_a -> unparseBlip 105
-    V.PP_ProFilm_psychedelic_blue_red -> unparseBlip 101
-    V.PP_space_woosh -> unparseBlip 110
+    V.PP_ProFilm_a -> unparseBlip' 96
+    V.PP_ProFilm_b -> unparseBlip' 96
+    V.PP_video_a -> unparseBlip' 107
+    V.PP_film_16mm -> unparseBlip' 98
+    V.PP_shitty_tv -> unparseBlip' 109
+    V.PP_bloom -> unparseBlip' 103
+    V.PP_film_sepia_ink -> unparseBlip' 99
+    V.PP_film_silvertone -> unparseBlip' 100
+    V.PP_film_b_w -> unparseBlip' 108
+    V.PP_video_bw -> unparseBlip' 108
+    V.PP_contrast_a -> unparseBlip' 97
+    V.PP_photocopy -> unparseBlip' 102
+    V.PP_film_blue_filter -> unparseBlip' 106
+    V.PP_desat_blue -> unparseBlip' 106
+    V.PP_video_security -> unparseBlip' 109
+    V.PP_bright -> unparseBlip' 104
+    V.PP_posterize -> unparseBlip' 103
+    V.PP_clean_trails -> unparseBlip' 110
+    V.PP_video_trails -> unparseBlip' 110
+    V.PP_flicker_trails -> unparseBlip' 110
+    V.PP_desat_posterize_trails -> unparseBlip' 98
+    V.PP_film_contrast -> unparseBlip' 97
+    V.PP_film_contrast_blue -> unparseBlip' 106
+    V.PP_film_contrast_green -> unparseBlip' 97
+    V.PP_film_contrast_red -> unparseBlip' 97
+    V.PP_horror_movie_special -> unparseBlip' 101
+    V.PP_photo_negative -> unparseBlip' 101
+    V.PP_ProFilm_mirror_a -> unparseBlip' 105
+    V.PP_ProFilm_psychedelic_blue_red -> unparseBlip' 101
+    V.PP_space_woosh -> unparseBlip' 110
   lighting l = case l of
     V.Lighting_ -> unparseCommand l
     V.Lighting_intro -> unparseCommand V.Lighting_
@@ -202,7 +219,7 @@ convertVenueInstant evts = let
   far = cut [70, 72]
   close = cut [70, 71, 73]
   cut :: [Int] -> [Instrument] -> RTB.T U.Beats E.T
-  cut distance insts = foldr RTB.merge RTB.empty $ map unparseBlip $
+  cut distance insts = foldr RTB.merge RTB.empty $ map unparseBlip' $
     60 : (distance ++ map cutInst insts)
   cutInst = \case
     Bass -> 61
@@ -299,3 +316,48 @@ convertVenueInstant evts = let
       V.Camera_directed_duo_kb -> directed "directed_duo_gb"
       V.Camera_directed_duo_kg -> directed "directed_duo_gb"
   in foldr RTB.merge cameraEvents $ rights changed
+
+fixFiveColors :: RTB.T U.Beats Five.Event -> RTB.T U.Beats Five.Event
+fixFiveColors rtb = let
+  getDiff d = RTB.partitionMaybe $ \case
+    Five.DiffEvent d' (Five.Note ln) | d == d' -> Just ln
+    _                                          -> Nothing
+  (easy  , notEasy  ) = getDiff Easy rtb
+  (medium, notMedium) = getDiff Medium notEasy
+  (hard  , notHard  ) = getDiff Hard notMedium
+  (expert, _        ) = getDiff Expert notHard
+  usedColors = Set.fromList $ concatMap toList $ RTB.getBodies expert
+  easy'   = makeDiff Easy   $ useColors usedColors easy
+  medium' = makeDiff Medium $ useColors usedColors medium
+  hard'   = makeDiff Hard   $ useColors usedColors hard
+  makeDiff d = fmap $ Five.DiffEvent d . Five.Note
+  in foldr RTB.merge notHard [easy', medium', hard']
+
+useColors :: Set.Set Five.Color -> RTB.T U.Beats (LongNote () Five.Color) -> RTB.T U.Beats (LongNote () Five.Color)
+useColors cols rtb = let
+  gtr = joinEdges $ Five.guitarify rtb
+  present = Set.fromList $ concatMap toList $ RTB.getBodies rtb
+  missing = Set.difference cols present
+  good = foldl (>>=) [gtr] $ map useColor $ Set.toDescList missing
+  in if Set.null missing then rtb else case good of
+    [] -> rtb
+    g : _ -> RTB.flatten $ fmap (traverse toList) $ splitEdges g
+
+focuses :: [a] -> [([a], a, [a])]
+focuses [] = []
+focuses xs = zip3 (inits xs) xs (tail $ tails xs)
+
+useColor
+  :: Five.Color
+  ->  RTB.T U.Beats ((), [Five.Color], Maybe U.Beats)
+  -> [RTB.T U.Beats ((), [Five.Color], Maybe U.Beats)]
+useColor newColor rtb = do
+  -- TODO sort this better (move closer colors first)
+  (before, (t, ((), oldColors, len)), after) <- focuses $ reverse $ RTB.toPairList rtb
+  oldColor <- oldColors
+  let newColors = map (\c -> if c == oldColor then newColor else c) oldColors
+  guard $ elem oldColor $ concatMap (\(_, (_, cols, _)) -> cols) $ before ++ after
+  return $ RTB.fromPairList $ reverse $ before ++ [(t, ((), newColors, len))] ++ after
+
+fixDrumColors :: RTB.T U.Beats Drums.Event -> RTB.T U.Beats Drums.Event
+fixDrumColors = id -- TODO
