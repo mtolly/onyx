0ca5d1a4ad01333008d91eee7c25f3bf290b640a
Michael Tolly <miketolly@gmail.com>
2017-12-07 12:35:35 -0600

    Fix `stack ghci` with Paths_ module

---
+++ haskell/.ghci-only/Paths_onyxite_customs_tool.hs
@@ -0,0 +1,9 @@
+module Paths_onyxite_customs_tool where
+
+-- thanks to a commenter on
+-- http://neilmitchell.blogspot.com/2008/02/adding-data-files-using-cabal.html
+
+import Data.Version (Version(..))
+
+version :: Version
+version = Version [1] []
