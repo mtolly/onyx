766d134913b1053350c71e9c3509bb9be4d22317
Michael Tolly <miketolly@gmail.com>
2019-03-24 18:35:39 -0500

    Split off GUI and Main modules from rest of the code
    This fixes the Windows build (TH) and Mac ghci,
    which both don't work with the fltk dependency currently

--- haskell/src/MoggDecrypt.hs
+++ haskell/src/MoggDecrypt.hs
@@ -1,26 +1,15 @@
-{-# LANGUAGE CPP        #-}
-{-# LANGUAGE LambdaCase #-}
 module MoggDecrypt (moggToOgg) where
 
 import           Control.Monad.IO.Class         (MonadIO (liftIO))
 import           Control.Monad.Trans.StackTrace
-#ifdef MOGGDECRYPT
-import qualified Sound.MOGG
-#else
 import           Control.Applicative            (liftA2)
 import           Data.Binary.Get                (getWord32le, runGet)
 import qualified Data.ByteString.Lazy           as BL
 import           Data.Char                      (toUpper)
 import           Numeric                        (showHex)
-#endif
 
--- | Only supports unencrypted MOGGs, unless you provide an external decryptor.
+-- | Just strips the header off an unencrypted MOGG for now.
 moggToOgg :: (MonadIO m) => FilePath -> FilePath -> StackTraceT m ()
-#ifdef MOGGDECRYPT
-moggToOgg mogg ogg = liftIO (Sound.MOGG.moggToOgg mogg ogg) >>= \case
-  True  -> return ()
-  False -> fatal $ "Couldn't decrypt MOGG: " ++ mogg
-#else
 moggToOgg mogg ogg = do
   bs <- liftIO $ BL.readFile mogg
   let (moggType, oggStart) = runGet (liftA2 (,) getWord32le getWord32le) bs
@@ -28,4 +17,3 @@ moggToOgg mogg ogg = do
   if moggType == 0xA
     then liftIO $ BL.writeFile ogg $ BL.drop (fromIntegral oggStart) bs
     else fatal $ "moggToOgg: encrypted MOGG (type " <> hex <> ") not supported"
-#endif
