303ac6bf1d0fedb3daa10d5b614efff7f3dc1665
Michael Tolly <miketolly@gmail.com>
2019-05-03 12:47:42 -0500

    Resurrect rhythm game demo app as separate exe

---
+++ haskell/src-game/Main.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+module Main where
+
+import           Build                            (loadYaml)
+import           Config
+import           Control.Arrow                    (first)
+import           Control.Concurrent               (threadDelay)
+import           Control.Exception                (bracket, bracket_, throwIO)
+import           Control.Monad.IO.Class           (liftIO)
+import           Control.Monad.Trans.Resource     (runResourceT)
+import           Control.Monad.Trans.StackTrace
+import qualified Data.EventList.Absolute.TimeBody as ATB
+import qualified Data.EventList.Relative.TimeBody as RTB
+import qualified Data.HashMap.Strict              as HM
+import qualified Data.Map.Strict                  as Map
+import           Data.Maybe                       (fromMaybe)
+import           Import                           (importSTFS)
+import qualified RhythmGame.Audio                 as RGAudio
+import qualified RhythmGame.Drums                 as RGDrums
+import           RockBand.Codec.Drums
+import qualified RockBand.Codec.File              as RBFile
+import           RockBand.Common                  (Difficulty (..))
+import           Scripts                          (loadMIDI)
+import           SDL                              (($=))
+import qualified SDL
+import qualified Sound.MIDI.Util                  as U
+import           System.Environment               (getArgs)
+import           System.FilePath                  ((</>))
+
+main :: IO ()
+main = getArgs >>= \case
+  [con] -> do
+    res <- runResourceT $ logStdout $ tempDir "onyx_game" $ \dir -> do
+      _ <- importSTFS con Nothing dir
+      song <- loadMIDI $ dir </> "notes.mid"
+      let tempos = RBFile.s_tempos song
+          drums = RBFile.fixedPartDrums $ RBFile.s_tracks song
+          drums'
+            = Map.fromList
+            $ map (first $ realToFrac . U.applyTempoMap tempos)
+            $ ATB.toPairList
+            $ RTB.toAbsoluteEventList 0
+            $ RTB.collectCoincident
+            $ fmap RGDrums.Upcoming
+            $ drumGems
+            $ fromMaybe mempty
+            $ Map.lookup Expert
+            $ drumDifficulties drums
+      yml <- loadYaml $ dir </> "song.yml"
+      (pans, vols) <- case HM.toList $ _plans yml of
+        [(_, MoggPlan{..})] -> return (map realToFrac _pans, map realToFrac _vols)
+        _                   -> fatal "Couldn't find pans and vols after importing STFS"
+      let trk = RGDrums.Track drums' Map.empty 0 0.2
+      liftIO $ bracket_ SDL.initializeAll SDL.quit $ do
+        let windowConf = SDL.defaultWindow
+              { SDL.windowResizable = True
+              , SDL.windowHighDPI = False
+              , SDL.windowInitialSize = SDL.V2 800 600
+              }
+        bracket (SDL.createWindow "Onyx" windowConf) SDL.destroyWindow $ \window -> do
+          SDL.windowMinimumSize window $= SDL.V2 800 600
+          bracket (SDL.createRenderer window (-1) SDL.defaultRenderer) SDL.destroyRenderer $ \rend -> do
+            threadDelay 1000000 -- this prevents a weird crash, see https://github.com/haskell-game/sdl2/issues/176
+            RGAudio.playMOGG pans vols (dir </> "audio.mogg") $ do
+              RGDrums.playDrums window rend trk
+    case res of
+      Left err -> throwIO err
+      Right () -> return ()
+  _ -> error "Usage: onyx-game song_rb3con"
