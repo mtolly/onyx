e4653f2f663d6ecf5ef83b886ef078bdf745bee3
Michael Tolly <miketolly@gmail.com>
2019-12-26 00:56:30 -0600

    SDL player can pause and restart

--- haskell/game/src/Main.hs
+++ haskell/game/src/Main.hs
@@ -7,10 +7,11 @@ import           Build                          (loadYaml)
 import           Config
 import           Control.Concurrent             (threadDelay)
 import           Control.Exception              (bracket, bracket_, throwIO)
-import           Control.Monad                  (forM, forM_, void, when)
+import           Control.Monad                  (forM, forM_, void)
 import           Control.Monad.IO.Class         (liftIO)
 import           Control.Monad.Trans.Resource   (runResourceT)
 import           Control.Monad.Trans.StackTrace
+import           Data.Fixed
 import qualified Data.HashMap.Strict            as HM
 import qualified Data.Text                      as T
 import           Graphics.GL.Core33
@@ -57,32 +58,64 @@ main = getArgs >>= \case
         bracket (SDL.createWindow "Onyx" windowConf) SDL.destroyWindow $ \window -> do
           SDL.windowMinimumSize window $= SDL.V2 800 600
           bracket (SDL.glCreateContext window) (\ctx -> glFinish >> SDL.glDeleteContext ctx) $ \_ctx -> do
-            RGAudio.playMOGG pans vols (dir </> "audio.mogg") $ do
-              playTracks window trks
+            RGAudio.withAL $ do
+              RGAudio.withMOGG (dir </> "audio.mogg") $ \ogg -> do
+                playTracks window trks pans vols ogg
     case res of
       Left err -> throwIO err
       Right () -> return ()
 
   _ -> error "Usage: onyx-game song_rb3con [track_number]"
 
-playTracks :: SDL.Window -> [PreviewTrack] -> IO ()
-playTracks window trks = do
-  initTime <- SDL.ticks
+data AppState = AppState
+  { songTime       :: Milli
+  , sdlStartedPlay :: Maybe (Milli, IO ())
+  }
+
+playTracks :: SDL.Window -> [PreviewTrack] -> [Float] -> [Float] -> FilePath -> IO ()
+playTracks window trks pans vols ogg = do
   glStuff <- loadGLStuff
-  let loop = do
-        frameStart <- fromIntegral <$> SDL.ticks
-        SDL.pollEvents >>= processEvents >>= \b -> when b $ do
-          timestamp <- SDL.ticks
-          draw $ fromIntegral (timestamp - initTime) / 1000
-          frameEnd <- fromIntegral <$> SDL.ticks
-          threadDelay $ (16 - (frameEnd - frameStart)) * 1000
-          loop
-      processEvents [] = return True
-      processEvents (e : es) = case SDL.eventPayload e of
-        SDL.QuitEvent -> return False
-        _             -> processEvents es
+  let ticksMilli :: IO Milli
+      ticksMilli = MkFixed . fromIntegral <$> SDL.ticks
+      delayMilli :: Milli -> IO ()
+      delayMilli (MkFixed m) = threadDelay $ fromIntegral m * 1000
+      loop prevState = do
+        frameStart <- ticksMilli
+        SDL.pollEvents >>= processEvents prevState >>= \case
+          Nothing -> return ()
+          Just appState -> do
+            timestamp <- ticksMilli
+            draw $ case sdlStartedPlay appState of
+              Nothing       -> songTime appState
+              Just (tks, _) -> songTime appState + (timestamp - tks)
+            frameEnd <- ticksMilli
+            delayMilli $ 0.016 - (frameEnd - frameStart)
+            loop appState
+      processEvents s [] = return $ Just s
+      processEvents s (e : es) = case SDL.eventPayload e of
+        SDL.QuitEvent -> return Nothing
+        SDL.KeyboardEvent SDL.KeyboardEventData
+          { SDL.keyboardEventKeyMotion = SDL.Pressed
+          , SDL.keyboardEventRepeat = False
+          , SDL.keyboardEventKeysym = SDL.Keysym
+            { SDL.keysymScancode = SDL.ScancodeSpace
+            }
+          } -> do
+            s' <- case sdlStartedPlay s of
+              Nothing -> do
+                ca <- RGAudio.sourceOGGFrom (realToFrac $ songTime s) ogg
+                stop <- RGAudio.playSource pans vols ca
+                timestamp <- ticksMilli
+                return s { sdlStartedPlay = Just (timestamp, stop) }
+              Just (started, stop) -> do
+                timestamp <- ticksMilli
+                let current = songTime s + (timestamp - started)
+                stop
+                return s { songTime = current, sdlStartedPlay = Nothing }
+            processEvents s' es
+        _             -> processEvents s es
       draw t = do
         SDL.V2 w h <- fmap fromIntegral <$> SDL.glGetDrawableSize window
-        drawTracks glStuff (WindowDims w h) t trks
+        drawTracks glStuff (WindowDims w h) (realToFrac t) trks
         SDL.glSwapWindow window
-  loop
+  loop $ AppState { songTime = 0, sdlStartedPlay = Nothing }
--- haskell/game/src/RhythmGame/Audio.hs
+++ haskell/game/src/RhythmGame/Audio.hs
@@ -3,18 +3,19 @@
 module RhythmGame.Audio where
 
 import           Control.Concurrent             (threadDelay)
-import           Control.Concurrent.Async       (withAsync)
+import           Control.Concurrent.Async       (async)
 import           Control.Concurrent.MVar
-import           Control.Exception              (throwIO)
-import           Control.Monad                  (forM_, when)
+import           Control.Exception              (bracket, throwIO)
+import           Control.Monad                  (forM_)
 import           Control.Monad.IO.Class         (liftIO)
 import           Control.Monad.Trans.Resource
 import           Control.Monad.Trans.StackTrace (logStdout)
 import           Data.Conduit                   (runConduit, (.|))
 import qualified Data.Conduit                   as C
 import qualified Data.Conduit.Audio             as CA
-import           Data.Conduit.Audio.Sndfile     (sourceSnd)
+import           Data.Conduit.Audio.Sndfile     (sourceSndFrom)
 import           Data.Int                       (Int16)
+import           Data.IORef
 import qualified Data.Vector.Storable           as V
 import           Foreign                        (Ptr, sizeOf)
 import           Foreign.C                      (CFloat (..))
@@ -24,56 +25,88 @@ import qualified Sound.OpenAL                   as AL
 import           System.FilePath                ((</>))
 import           System.IO.Temp
 
-playMOGG :: [Float] -> [Float] -> FilePath -> IO a -> IO a
-playMOGG pans vols mogg fn = withSystemTempDirectory "onyxPlayMogg" $ \tmp -> do
+withMOGG :: FilePath -> (FilePath -> IO a) -> IO a
+withMOGG mogg fn = withSystemTempDirectory "onyxLoadMogg" $ \tmp -> do
   let ogg = tmp </> "audio.ogg"
   logStdout (moggToOgg mogg ogg) >>= either throwIO return
+  fn ogg
+
+sourceOGGFrom :: Double -> FilePath -> IO (CA.AudioSource (ResourceT IO) Int16)
+sourceOGGFrom pos ogg = CA.mapSamples CA.integralSample <$>
   -- this should be doable with libsndfile's clamping but it doesn't work for some reason?
-  ca <- CA.mapSamples CA.integralSample <$> (sourceSnd ogg :: IO (CA.AudioSource (ResourceT IO) Float))
+  (sourceSndFrom (CA.Seconds pos) ogg :: IO (CA.AudioSource (ResourceT IO) Float))
+
+withAL :: IO a -> IO a
+withAL fn = let
+  openDevice = AL.openDevice Nothing >>= maybe (error "couldn't open audio") return
+  createContext dev = AL.createContext dev [] >>= maybe (error "couldn't create context") return
+  destroyContext ctx = do
+    AL.currentContext $= Nothing
+    AL.destroyContext ctx
+  in bracket openDevice AL.closeDevice $ \dev -> do
+    bracket (createContext dev) destroyContext $ \ctx -> do
+      AL.currentContext $= Just ctx
+      fn
+
+data AudioState = Filling | Playing | Stopping
+
+playSource
+  :: [Float]
+  -> [Float]
+  -> CA.AudioSource (ResourceT IO) Int16
+  -> IO (IO ())
+playSource pans vols ca = do
   let chanCount = CA.channels ca
       floatRate = realToFrac $ CA.rate ca
-  dev <- AL.openDevice Nothing >>= maybe (error "couldn't open audio") return
-  ctx <- AL.createContext dev [] >>= maybe (error "couldn't create context") return
-  AL.currentContext $= Just ctx
   srcs <- AL.genObjectNames chanCount
   forM_ (zip srcs pans) $ \(src, pan) -> AL.sourcePosition src $= AL.Vertex3 (CFloat pan) 0 0
   forM_ (zip srcs vols) $ \(src, volDB) -> AL.sourceGain src $= CFloat (10 ** (volDB / 20))
   firstFull <- newEmptyMVar
+  stopper <- newIORef False
   let queueSize = 5
       waitTime = 10000 -- 0.01 secs
       t1 = runResourceT $ runConduit $ CA.source ca .| let
-        loop reportFull = do
-          current <- liftIO $ AL.buffersQueued $ head srcs
-          if current < queueSize
-            then do
-              -- liftIO $ putStrLn $ "Filling because queue has " <> show current
-              C.await >>= \case
-                Nothing -> return ()
-                Just chunk -> do
-                  bufs <- AL.genObjectNames chanCount
-                  forM_ (zip bufs $ CA.deinterleave chanCount chunk) $ \(buf, chan) -> do
-                    liftIO $ V.unsafeWith chan $ \p -> do
-                      let _ = p :: Ptr Int16
-                      AL.bufferData buf $= AL.BufferData
-                        (AL.MemoryRegion p $ fromIntegral $ V.length chan * sizeOf (V.head chan))
-                        AL.Mono16
-                        floatRate
-                  forM_ (zip srcs bufs) $ \(src, buf) -> do
-                    AL.queueBuffers src [buf]
-              loop reportFull
-            else do
-              -- liftIO $ putStrLn "Queue is full"
-              when reportFull $ liftIO $ putMVar firstFull ()
-              forM_ srcs $ \src -> liftIO (AL.buffersProcessed src) >>= \case
-                0 -> return ()
-                n -> do
-                  -- liftIO $ putStrLn $ "Removing " <> show n <> " finished buffers"
-                  bufs <- AL.unqueueBuffers src n
-                  AL.deleteObjectNames bufs
-              liftIO $ threadDelay waitTime
-              loop False
-        in loop True
-  withAsync t1 $ \_ -> do
-    () <- takeMVar firstFull
-    AL.play srcs
-    fn
+        loop audioState = liftIO (readIORef stopper) >>= \case
+          True -> do
+            case audioState of
+              Stopping -> return ()
+              _        -> AL.stop srcs
+            -- TODO remove all the sources' buffers, waiting if necessary
+            return ()
+          False -> do
+            current <- liftIO $ AL.buffersQueued $ head srcs
+            if current < queueSize
+              then do
+                -- liftIO $ putStrLn $ "Filling because queue has " <> show current
+                C.await >>= \case
+                  Nothing -> return ()
+                  Just chunk -> do
+                    bufs <- AL.genObjectNames chanCount
+                    forM_ (zip bufs $ CA.deinterleave chanCount chunk) $ \(buf, chan) -> do
+                      liftIO $ V.unsafeWith chan $ \p -> do
+                        let _ = p :: Ptr Int16
+                        AL.bufferData buf $= AL.BufferData
+                          (AL.MemoryRegion p $ fromIntegral $ V.length chan * sizeOf (V.head chan))
+                          AL.Mono16
+                          floatRate
+                    forM_ (zip srcs bufs) $ \(src, buf) -> do
+                      AL.queueBuffers src [buf]
+                loop audioState
+              else do
+                -- liftIO $ putStrLn "Queue is full"
+                case audioState of
+                  Filling -> liftIO $ putMVar firstFull ()
+                  _       -> return ()
+                forM_ srcs $ \src -> liftIO (AL.buffersProcessed src) >>= \case
+                  0 -> return ()
+                  n -> do
+                    -- liftIO $ putStrLn $ "Removing " <> show n <> " finished buffers"
+                    bufs <- AL.unqueueBuffers src n
+                    AL.deleteObjectNames bufs
+                liftIO $ threadDelay waitTime
+                loop Playing
+        in loop Filling
+  _ <- async t1
+  () <- takeMVar firstFull
+  AL.play srcs
+  return $ writeIORef stopper True
