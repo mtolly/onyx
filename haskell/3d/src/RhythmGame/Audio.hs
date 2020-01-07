7e9929befa1ad37e2a9c61c18507f0f7afe55220
Michael Tolly <miketolly@gmail.com>
2020-01-07 13:30:31 -0600

    Fix remaining audio bugs
    Some fixed by switching from Apple OpenAL to openal-soft on Mac

--- haskell/3d/src/RhythmGame/Audio.hs
+++ haskell/3d/src/RhythmGame/Audio.hs
@@ -7,7 +7,7 @@ import           Control.Concurrent             (threadDelay)
 import           Control.Concurrent.Async       (async)
 import           Control.Concurrent.MVar
 import           Control.Exception              (bracket, throwIO)
-import           Control.Monad                  (forM_, filterM, when)
+import           Control.Monad                  (forM, forM_)
 import           Control.Monad.IO.Class         (liftIO)
 import           Control.Monad.Trans.Resource
 import           Control.Monad.Trans.StackTrace (logStdout)
@@ -17,6 +17,7 @@ import qualified Data.Conduit.Audio             as CA
 import           Data.Conduit.Audio.Sndfile     (sourceSndFrom)
 import           Data.Int                       (Int16)
 import           Data.IORef
+import qualified Data.Set                       as Set
 import qualified Data.Vector.Storable           as V
 import           Foreign                        (Ptr, sizeOf)
 import           Foreign.C                      (CFloat (..))
@@ -26,8 +27,23 @@ import qualified Sound.OpenAL                   as AL
 import           System.FilePath                ((</>))
 import           System.IO.Temp
 
--- TODO all OpenAL functions should be wrapped in error checking!
--- probably need to use a lock since there's only one shared error value
+{-
+{-# NOINLINE lockAL #-}
+lockAL :: MVar ()
+lockAL = unsafePerformIO $ newMVar ()
+
+checkAL :: String -> IO a -> IO a
+checkAL desc f = withMVar lockAL $ \() -> do
+  _ <- AL.get AL.alErrors
+  x <- f
+  errs <- AL.get AL.alErrors
+  unless (null errs) $ putStrLn $ desc <> ": " <> show errs
+  return x
+-}
+
+-- | Can be swapped out with checkAL to see OpenAL errors
+doAL :: String -> IO a -> IO a
+doAL _ f = f
 
 withMOGG :: FilePath -> (FilePath -> IO a) -> IO a
 withMOGG mogg fn = withSystemTempDirectory "onyxLoadMogg" $ \tmp -> do
@@ -55,23 +71,29 @@ withAL fn = let
 
 data AudioState = Filling | Playing
 
+{-
 emptySources :: [AL.Source] -> IO ()
 emptySources srcs = do
   srcs' <- flip filterM srcs $ \src -> do
-    cur <- AL.buffersQueued src
-    proc <- AL.buffersProcessed src
+    cur <- doAL "emptySources buffersQueued" $ AL.buffersQueued src
+    proc <- doAL "emptySources buffersProcessed" $ AL.buffersProcessed src
     if cur == 0
       then do
-        AL.deleteObjectNames [src]
+        doAL "emptySources deleteObjectNames source" $ AL.deleteObjectNames [src]
         return False
       else do
-        when (proc /= 0) $ AL.unqueueBuffers src proc >>= AL.deleteObjectNames
+        -- this runs into problems because sometimes an unqueued buffer still
+        -- can't be deleted for a bit! that's why we don't use this anymore
+        when (proc /= 0) $ doAL "emptySources unqueueBuffers" (AL.unqueueBuffers src proc)
+          >>= doAL "emptySources deleteObjectNames buffers" . AL.deleteObjectNames
         return True
   case srcs' of
     [] -> return ()
     _ -> do
+      putStrLn $ "Waiting for " <> show (length srcs') <> " to empty"
       threadDelay 5000
       emptySources srcs'
+-}
 
 playSource
   :: [Float]
@@ -81,52 +103,61 @@ playSource
 playSource pans vols ca = do
   let chanCount = CA.channels ca
       floatRate = realToFrac $ CA.rate ca
-  srcs <- AL.genObjectNames chanCount
-  forM_ (zip srcs pans) $ \(src, pan) -> AL.sourcePosition src $= AL.Vertex3 (CFloat pan) 0 0
-  forM_ (zip srcs vols) $ \(src, volDB) -> AL.sourceGain src $= CFloat (10 ** (volDB / 20))
+  srcs <- doAL "playSource genObjectNames sources" $ AL.genObjectNames chanCount
+  forM_ (zip srcs pans) $ \(src, pan) ->
+    doAL "playSource setting sourcePosition" $
+    AL.sourcePosition src $= AL.Vertex3 (CFloat pan) 0 0
+  forM_ (zip srcs vols) $ \(src, volDB) ->
+    doAL "playSource setting sourceGain" $
+    AL.sourceGain src $= CFloat (10 ** (volDB / 20))
   firstFull <- newEmptyMVar
   stopper <- newIORef False
+  stopped <- newEmptyMVar
   let queueSize = 7
       waitTime = 5000
       t1 = runResourceT $ runConduit $ CA.source ca .| let
-        loop audioState = liftIO (readIORef stopper) >>= \case
-          True -> do
-            AL.stop srcs
-            liftIO $ emptySources srcs
+        loop currentBuffers audioState = liftIO (readIORef stopper) >>= \case
+          True -> liftIO $ do
+            doAL "playSource stopping sources" $ AL.stop srcs
+            doAL "playSource deleting sources" $ AL.deleteObjectNames srcs
+            doAL "playSource deleting remaining buffers" $ AL.deleteObjectNames $ Set.toList currentBuffers
+            putMVar stopped ()
           False -> do
-            current <- liftIO $ AL.buffersQueued $ head srcs
+            current <- liftIO $ doAL "playSource buffersQueued" $ AL.buffersQueued $ head srcs
             if current < queueSize
               then do
                 -- liftIO $ putStrLn $ "Filling because queue has " <> show current
-                C.await >>= \case
-                  Nothing -> return ()
+                newBuffers <- C.await >>= \case
+                  Nothing -> return currentBuffers
                   Just chunk -> do
-                    bufs <- AL.genObjectNames chanCount
+                    bufs <- liftIO $ doAL "playSource genObjectNames buffers" $ AL.genObjectNames chanCount
                     forM_ (zip bufs $ CA.deinterleave chanCount chunk) $ \(buf, chan) -> do
                       liftIO $ V.unsafeWith chan $ \p -> do
                         let _ = p :: Ptr Int16
-                        AL.bufferData buf $= AL.BufferData
+                        doAL "playSource set bufferData" $ AL.bufferData buf $= AL.BufferData
                           (AL.MemoryRegion p $ fromIntegral $ V.length chan * sizeOf (V.head chan))
                           AL.Mono16
                           floatRate
                     forM_ (zip srcs bufs) $ \(src, buf) -> do
-                      AL.queueBuffers src [buf]
-                loop audioState
+                      liftIO $ doAL "playSource queueBuffers" $ AL.queueBuffers src [buf]
+                    return $ Set.fromList bufs
+                loop (Set.union currentBuffers newBuffers) audioState
               else do
                 -- liftIO $ putStrLn "Queue is full"
                 case audioState of
                   Filling -> liftIO $ putMVar firstFull ()
                   _       -> return ()
-                forM_ srcs $ \src -> liftIO (AL.buffersProcessed src) >>= \case
-                  0 -> return ()
+                removedBuffers <- fmap Set.unions $ liftIO $ forM srcs $ \src -> doAL "playSource buffersProcessed" (AL.buffersProcessed src) >>= \case
+                  0 -> return Set.empty
                   n -> do
                     -- liftIO $ putStrLn $ "Removing " <> show n <> " finished buffers"
-                    bufs <- AL.unqueueBuffers src n
-                    AL.deleteObjectNames bufs
+                    bufs <- doAL "playSource unqueueBuffers" $ AL.unqueueBuffers src n
+                    doAL "playSource deleteObjectNames buffers" $ AL.deleteObjectNames bufs
+                    return $ Set.fromList bufs
                 liftIO $ threadDelay waitTime
-                loop Playing
-        in loop Filling
+                loop (Set.difference currentBuffers removedBuffers) Playing
+        in loop Set.empty Filling
   _ <- async t1
   () <- takeMVar firstFull
-  AL.play srcs
-  return $ writeIORef stopper True
+  doAL "playSource play" $ AL.play srcs
+  return $ writeIORef stopper True >> takeMVar stopped
