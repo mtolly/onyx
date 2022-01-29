mtolly/onyx

commit dd66f3b5ef070164df9db921389e1a3c79c2e868

Charts; start on WoR .perf format; fix official TBRB import

haskell/src/Data/DTA/Serialize/RB3.hs
@@ -148,7 +148,7 @@ instance StackChunks SongPackage where
     songScrollSpeed   <- songScrollSpeed   =. req         "song_scroll_speed"   stackChunks
     bank              <- bank              =. opt Nothing "bank"                stackChunks
     drumBank          <- drumBank          =. opt Nothing "drum_bank"           (chunksMaybe $ single chunkSym) -- this has to be output as a symbol for C3
-    animTempo         <- animTempo         =. req         "anim_tempo"          stackChunks
+    animTempo         <- animTempo         =. opt (Left KTempoMedium) "anim_tempo" stackChunks
     songLength        <- songLength        =. opt Nothing "song_length"         stackChunks
     preview           <- preview           =. req         "preview"             stackChunks
     rank              <- rank              =. req         "rank"                (chunksDict chunkSym stackChunks)

haskell/src/Neversoft/Export.hs
@@ -17,10 +17,10 @@ import qualified Data.ByteString.Lazy             as BL
 import           Data.Char                        (toLower, toUpper)
 import qualified Data.EventList.Absolute.TimeBody as ATB
 import qualified Data.EventList.Relative.TimeBody as RTB
+import           Data.Foldable                    (toList)
 import qualified Data.HashMap.Strict              as HM
 import           Data.List                        (sort)
 import qualified Data.Map                         as Map
-import Data.Foldable (toList)
 import           Data.Maybe                       (catMaybes, fromMaybe,
                                                    isNothing)
 import qualified Data.Set                         as Set
@@ -69,7 +69,8 @@ import           RockBand3                        (BasicTiming (..),
 import qualified Sound.MIDI.Util                  as U
 import           STFS.Package
 import qualified System.Directory                 as Dir
-import           System.FilePath                  (takeExtension, (<.>), (</>), takeFileName)
+import           System.FilePath                  (takeExtension, takeFileName,
+                                                   (<.>), (</>))
 import           System.IO.Temp                   (withSystemTempDirectory)
 
 worGuitarEdits

haskell/src/Neversoft/Perf.hs
@@ -1,0 +1,149 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+module Neversoft.Perf where
+
+import           Control.Monad
+import           Data.Binary.Get
+import qualified Data.ByteString      as B
+import qualified Data.ByteString.Lazy as BL
+import           Data.Word            (Word32)
+import           Neversoft.Checksum
+import           STFS.Package         (runGetM)
+
+data Perf = Perf
+  { perfDLCKey   :: Word32
+  , perfUnknown1 :: Word32
+  , perfUnknown2 :: Word32
+  , perfUnknown3 :: Word32
+  , perfEntries  :: [PerfEntry]
+  } deriving (Show)
+
+data PerfEntry
+  = PerfGH5CameraNote Word32 [(Word32, B.ByteString)]
+  | PerfGH6ActorLoops Word32 [B.ByteString]
+  deriving (Show)
+
+getPerfEntry :: Get PerfEntry
+getPerfEntry = do
+  entryID <- getWord32be
+  -- entryID for gh5_camera_note is either "autocutcameras" or "momentcameras".
+  -- for gh6_actor_loops, entryID appears to be a unique ID
+  len <- fromIntegral <$> getWord32be
+  qb <- getWord32be
+  if qb == qbKeyCRC "gh5_camera_note"
+    then fmap (PerfGH5CameraNote entryID) $ replicateM len $ do
+      t <- getWord32be
+      x <- getByteString 3
+      return (t, x)
+    else if qb == qbKeyCRC "gh6_actor_loops"
+      then fmap (PerfGH6ActorLoops entryID) $ replicateM len $ do
+        getByteString 1000
+      else fail "Unknown .perf entry type"
+
+getPerf :: Get Perf
+getPerf = do
+  0x40A001A3 <- getWord32be
+  perfDLCKey <- getWord32be
+  numEntries <- getWord32be
+  0x424056AD <- getWord32be -- qb "perf"
+  perfUnknown1 <- getWord32be -- always 0x36?
+  perfUnknown2 <- getWord32be
+  perfUnknown3 <- getWord32be
+  perfEntries <- replicateM (fromIntegral numEntries) getPerfEntry
+  isEmpty >>= \b -> unless b $ fail "Expected EOF at end of .perf"
+  return Perf{..}
+
+loadPerf :: FilePath -> IO Perf
+loadPerf f = B.readFile f >>= runGetM getPerf . BL.fromStrict
+
+{-
+
+matching up camera cuts to video of dlc701 (Soundgarden - Hunted Down)
+https://www.youtube.com/watch?v=HE18lMbNAm0
+
+(0,[9,165,31]) behind the band, very slow pan left
+(2469,[23,18,45]) in crowd way far from band, zoom in slowly
+(8375,[10,43,10]) guitarist shot, pan over to vocalist
+  moment camera: (8375,[19,141,3])
+(10978,[9,98,9]) no cut here!
+(13380,[9,198,48]) front of band, up above
+(15883,[10,243,24]) drummer, close up from side
+(18685,[6,232,30]) far away, in the middle of crowd
+(20454,[8,21,23]) drummer, close up from front
+(22522,[8,154,18]) bassist, front
+(24725,[16,142,15]) behind vocalist and bassist
+(28962,[14,153,23]) behind drummer, rise up from below
+  moment camera: (28962,[14,153,68])
+(32699,[22,107,43]) guitarist closeup from front
+  moment camera: (32699,[22,107,3])
+(38438,[14,186,30]) far away, in middle of crowd
+(42209,[15,197,74]) in crowd, pan quickly from right to left
+(46246,[7,243,8]) guitarist
+  moment camera: (46246,[7,243,4])
+(48282,[24,196,13]) vocalist first line, dramatic
+  moment camera: (48282,[24,196,6])
+(54621,[8,121,10]) guitarist
+(56790,[17,253,43]) far in crowd, slightly to the left
+(61395,[19,8,18]) behind bassist, on floor, rise up slowly
+  moment camera: (61395,[19,8,65])
+(66266,[21,96,13]) behind vocalist
+  moment camera: (66266,[21,96,94])
+(71738,[7,210,23]) drummer, close up from front
+(73740,[12,131,30]) far away in crowd
+(76944,[12,98,11]) behind guitarist, can also see vox/bass
+(80113,[11,154,21]) behind bassist, can also see vox
+(83083,[8,87,28]) way far away up above crowd
+(85218,[7,143,26]) close up behind drummer
+(87154,[17,219,58]) behind band up above, slow camera pan down
+(91725,[7,177,13]) guitarist from side
+  moment camera: (91725,[15,64,5])
+(93694,[7,143,8]) no cut here!
+(95629,[13,9,43]) far off in crowd
+(98966,[8,54,13]) close up of guitarist from front
+  moment camera: (98966,[8,54,3])
+(101068,[14,186,60]) vocalist from side
+  moment camera: (101068,[14,186,5])
+(104838,[15,97,30]) guitarist fretboard shot
+  moment camera: (104838,[15,97,33])
+(108775,[20,152,11]) vocalist from front, slightly below
+  moment camera: (108775,[20,152,3])
+(114047,[18,230,20]) bassist from front, slightly far away
+  moment camera: (114047,[18,230,3])
+(118885,[10,209,25]) drummer from front, pan from left to right
+(121655,[8,121,10]) guitarist from front/side
+(123824,[13,142,28]) far off in crowd, up above
+(127294,[17,86,26]) vocalist from front
+  moment camera: (127294,[17,86,3])
+(131732,[10,43,18]) guitarist fretboard shot
+  moment camera: (131732,[10,43,33])
+(134334,[16,208,74]) in crowd, pan from left to right
+(138638,[12,231,13]) behind vocalist
+  moment camera: (138638,[12,231,4])
+(141942,[8,254,26]) behind drummer
+(144244,[8,54,10]) guitar, camera mounted to guitar body (looking up towards neck)
+  moment camera: (144244,[8,54,33])
+(146346,[14,53,16]) vocalist from front, can also see guitar
+  moment camera: (146346,[14,53,3])
+(149983,[9,132,48]) front of band from up above, zooms out quickly and then stops
+(152419,[11,20,23]) drummer close up from front
+(155255,[11,87,10]) guitar from side, can also see vocalist
+(158158,[11,54,26]) drummer from behind, slight pan from right to left
+(161028,[14,20,75]) far above crowd, pan from right to left
+(164631,[4,110,25]) drummer from side
+(165766,[28,39,46]) out above crowd, camera titled
+
+Comparing to https://www.youtube.com/watch?v=AO7LVVaNhvs
+Most are more or less the same, but a few angles are different,
+and the smaller venue reduces camera movement.
+
+-}
+
+{-
+actor loops:
+0x40 bytes of stuff
+0x88 bytes of zero (0x40 + 0x88 = 200)
+0x40 bytes of stuff
+0x88 bytes of zero (0x40 + 0x88 = 200)
+0x3c bytes of stuff
+0x21c bytes of zero (0x3c + 0x21c = 600)
+-}

haskell/src/PlayStation/PKG.hs
@@ -288,6 +288,9 @@ tryDecryptEDAT dir name readable = do
         "BLUS30463" -> do
           lg "Decrypting Rock Band 3 EDAT file"
           return $ Just $ rockBandKLIC dir
+        "BLUS30282" -> do
+          lg "Decrypting The Beatles: Rock Band EDAT file"
+          return $ Just $ rockBandKLIC dir
         _ -> do
           warn $ "Unknown KLIC for game ID " <> show gameID
           return Nothing

rs.fish
@@ -57,8 +57,9 @@ end
 # build songs-hmx/megadeth/hangar-18 # can't put on CF
 # build songs/john-myung/solar-groove
 # build songs/jizue/marten # needs 4-string
-build songs-others/guitar-hero/my-apocalypse
-build songs-others/guitar-hero/suicide-and-redemption-j-h
+# build songs-others/guitar-hero/my-apocalypse
+# build songs-others/guitar-hero/suicide-and-redemption-j-h
+build songs-others/bloodline/fish-amir-bresler-playthrough
 
 # phrases not done:
 
@@ -114,7 +115,7 @@ build songs-others/guitar-hero/suicide-and-redemption-j-h
 # build songs-others/cyclopsdragon/when-i-lost-my-bet
 # build songs/billy-cobham/stratus
 # build songs/tormented-brutality/dominate
-build songs-hmx/metallica/ride-the-lightning
+# build songs-hmx/metallica/ride-the-lightning
 
 # phrases unknown:
 

commit 8cedb004f0261e9fb12717bb1a3a8af2fa981305

Various bug fixes
- Commit dd66f3b caused anim_tempo to start being dropped, which crashes RB2 (but not RB3)
- Changing e.g. FiveTrack to store Edges for notes (instead of matched on/off) broke code that removes invalid OD phrases (note-off got treated as a note)
- Fix game demo compile

haskell/game/src/Main.hs
@@ -51,9 +51,9 @@ main = getArgs >>= \case
       let dir = takeDirectory $ projectLocation proj
       song <- loadTracks (projectSongYaml proj) $ dir </> "notes.mid"
       let allTracks = concat $ previewTracks song
-      drums <- case snd $ allTracks !! index of
-        PreviewDrumsFull drums -> return drums
-        _                      -> fatal "Not a DTX drums track"
+      (drums, layout) <- case snd $ allTracks !! index of
+        PreviewDrumsFull layout drums -> return (drums, layout)
+        _                             -> fatal "Not a DTX drums track"
       let planName = fst $ head $ HM.toList $ _plans $ projectSongYaml proj
       RGAudio.projectAudio planName proj >>= \case
         Nothing -> return ()
@@ -71,7 +71,7 @@ main = getArgs >>= \case
             SDL.windowMinimumSize window $= SDL.V2 800 600
             bracket (SDL.glCreateContext window) (\ctx -> glFinish >> SDL.glDeleteContext ctx) $ \_ctx -> do
               RGAudio.withAL $ \_openedAudio -> do
-                playDrumTrack window song drums audio
+                playDrumTrack window song layout drums audio
     case res of
       Left err -> throwIO err
       Right () -> return ()
@@ -140,10 +140,11 @@ startMIDIListen inner = do
 playDrumTrack
   :: SDL.Window
   -> PreviewSong
+  -> FullDrumLayout
   -> Map.Map Double (PNF.CommonState (PNF.DrumState FD.FullDrumNote FD.FullGem))
   -> (Double -> Maybe Double -> Float -> IO RGAudio.AudioHandle)
   -> IO ()
-playDrumTrack window song trk audioPlayer = do
+playDrumTrack window song layout trk audioPlayer = do
   Right glStuff <- logStdout $ loadGLStuff song
   let ticksMilli :: IO Milli
       ticksMilli = MkFixed . fromIntegral <$> SDL.ticks
@@ -169,7 +170,7 @@ playDrumTrack window song trk audioPlayer = do
               timestamp <- ticksMilli
               let t = timestamp - startedAt
               SDL.V2 w h <- fmap fromIntegral <$> SDL.glGetDrawableSize window
-              drawDrumPlayFull glStuff (WindowDims w h) (realToFrac t) 1 dps
+              drawDrumPlayFull glStuff (WindowDims w h) (realToFrac t) 1 layout dps
               SDL.glSwapWindow window
               frameEnd <- ticksMilli
               delayMilli $ 0.016 - (frameEnd - frameStart)

haskell/src-exe/GUI/FLTK.hs
@@ -137,6 +137,8 @@ import           Preferences                               (MagmaSetting (..),
                                                             readPreferences,
                                                             savePreferences)
 import           ProKeysRanges                             (closeShiftsFile)
+import           QuickConvert                              (QuickSong,
+                                                            loadInput)
 import           Reaper.Build                              (TuningInfo (..),
                                                             makeReaper)
 import           Reductions                                (simpleReduce)
@@ -1479,6 +1481,21 @@ makePresetDropdown rect opts = do
       ) :: Maybe (FL.Ref FL.MenuItem -> IO ()))
       (FL.MenuItemFlags [])
   return $ readIORef ref
+
+makeModeDropdown :: Rectangle -> [(T.Text, a)] -> (a -> IO ()) -> IO ()
+makeModeDropdown rect opts withOpt = do
+  let (initialLabel, initialOpt) = head opts
+  menu <- FL.menuButtonNew rect $ Just initialLabel
+  forM_ opts $ \(label, opt) -> do
+    -- need to escape in menu items (but not button label) to not make submenus
+    let label' = T.replace "/" "\\/" label
+    FL.add menu label' Nothing
+      ((Just $ \_ -> do
+        withOpt opt
+        FL.setLabel menu label
+      ) :: Maybe (FL.Ref FL.MenuItem -> IO ()))
+      (FL.MenuItemFlags [])
+  withOpt initialOpt
 
 data RB3Create
   = RB3CON FilePath
@@ -3624,6 +3641,15 @@ searchWoRCachable f = stackIO $ Dir.doesDirectoryExist f >>= \case
   True  -> (\fs -> (map (f </>) fs, [])) <$> Dir.listDirectory f
   False -> (\mspec -> ([], toList mspec)) <$> isWoRCachable f
 
+searchQuickSongs :: FilePath -> Onyx ([FilePath], [(FilePath, [QuickSong])])
+searchQuickSongs f = stackIO (Dir.doesDirectoryExist f) >>= \case
+  True  -> stackIO $ (\fs -> (map (f </>) fs, [])) <$> Dir.listDirectory f
+  False -> do
+    res <- errorToWarning $ loadInput f
+    case res of
+      Nothing     -> return ([], [])
+      Just qsongs -> return ([], [(f, qsongs)])
+
 isWoRCachable :: FilePath -> IO (Maybe (FilePath, T.Text))
 isWoRCachable f = if "_TEXT.PAK.PS3.EDAT" `T.isSuffixOf` T.toUpper (T.pack f)
   then return $ Just (f, "") -- could read content ID from EDAT if we wanted
@@ -3811,6 +3837,67 @@ miscPagePacks sink rect tab startTasks = mdo
   FL.setCallback btnPKG $ \_ -> doPKGPrompt
 
   mixedWarning <- FL.boxNew pkgRect $ Just "Mixed 360/PS3 packages provided!"
+
+  FL.setResizable tab $ Just group
+
+data QuickConvertMode
+  = QCInPlace
+  | QCOneToOne
+  | QCMakePacks
+  | QCMakeSongs
+
+pageQuickConvert
+  :: (Event -> IO ())
+  -> Rectangle
+  -> FL.Ref FL.Group
+  -> ([(String, Onyx [FilePath])] -> Onyx ())
+  -> IO ()
+pageQuickConvert sink rect tab startTasks = mdo
+  loadedFiles <- newMVar []
+  let (filesRect, bottomRect) = chopBottom 175 rect
+      [row1, row2, row3, row4] = map (trimClock 5 10 5 10) $ splitVertN 4 bottomRect
+  group <- fileLoadWindow filesRect sink "CON/LIVE/PKG" "CON/LIVE/PKG" (modifyMVar_ loadedFiles) [] searchQuickSongs
+    $ \info -> case info of
+      (f, qsongs) -> let
+        entry = T.pack f
+        subline = T.pack $ case length qsongs of
+          1 -> "1 song"
+          n -> show n <> " songs"
+        in (entry, [subline])
+
+  let withQCMode _ = sink $ EventOnyx $ do
+        return ()
+
+  -- row1: midi checkboxes
+  let [midiBox1, midiBox2, midiBox3] = splitHorizN 3 row1
+  checkBlackVenue <- FL.checkButtonNew midiBox1 $ Just "Black VENUE"
+  void $ FL.setValue checkBlackVenue False
+  checkNoOverdrive <- FL.checkButtonNew midiBox2 $ Just "Remove Overdrive"
+  void $ FL.setValue checkNoOverdrive False
+  checkNoLanes <- FL.checkButtonNew midiBox3 $ Just "Remove lanes"
+  void $ FL.setValue checkNoLanes False
+  -- row2: select mode
+  makeModeDropdown row2
+    [ ("Transform in place: replace inputs with new versions", QCInPlace)
+    , ("One to one: each input produces a new output file", QCOneToOne)
+    , ("Make packs: combine songs up to a maximum file size", QCMakePacks)
+    , ("Make songs: produce a single file for each song", QCMakeSongs)
+    ] withQCMode
+  -- row3/row4: mode-specific stuff
+  -- Transform in place (each input is replaced with a file, package type preserved)
+  --   for pkg, don't change encryption folders
+  --   2nd row: enc/unenc midi select
+  --   3rd row: go button
+  -- One to one (each input file gets one new output file, of a single package type)
+  --   for pkg->pkg, don't change encryption folders
+  --   2nd row: con/pkg/live, if pkg then (enc/unenc midi select, separate/combined songs.dta select)
+  --   3rd row: template box with default as %dir%/%base%-convert(.pkg), go button
+  -- Make packs (songs are combined into packs, up to a maximum size)
+  --   2nd row: con/pkg/live, if pkg then (enc/unenc midi select, separate/combined songs.dta select)
+  --   3rd row: max size, go button (opens a save-as dialog, used as pack name or template for multiple)
+  -- Make songs (each song gets one new output file)
+  --   2nd row: con/pkg/live, if pkg then (enc/unenc midi select)
+  --   3rd row: go button (opens pick folder dialog, songs get auto names inside)
 
   FL.setResizable tab $ Just group
 
@@ -4104,7 +4191,11 @@ launchQuickConvert sink makeMenuBar = mdo
   FL.begin window
   tabs <- FL.tabsNew windowRect Nothing
   functionTabs <- sequence
-    [ makeTab windowRect "Make a pack" $ \rect tab -> do
+    [ {- makeTab windowRect "Quick convert" $ \rect tab -> do
+      functionTabColor >>= setTabColor tab
+      pageQuickConvert sink rect tab startTasks
+      return tab
+    , -} makeTab windowRect "Make a pack" $ \rect tab -> do
       functionTabColor >>= setTabColor tab
       miscPagePacks sink rect tab startTasks
       return tab

haskell/src/Data/DTA/Serialize/RB3.hs
@@ -148,7 +148,7 @@ instance StackChunks SongPackage where
     songScrollSpeed   <- songScrollSpeed   =. req         "song_scroll_speed"   stackChunks
     bank              <- bank              =. opt Nothing "bank"                stackChunks
     drumBank          <- drumBank          =. opt Nothing "drum_bank"           (chunksMaybe $ single chunkSym) -- this has to be output as a symbol for C3
-    animTempo         <- animTempo         =. opt (Left KTempoMedium) "anim_tempo" stackChunks
+    animTempo         <- animTempo         =. fill (Left KTempoMedium) "anim_tempo" stackChunks
     songLength        <- songLength        =. opt Nothing "song_length"         stackChunks
     preview           <- preview           =. req         "preview"             stackChunks
     rank              <- rank              =. req         "rank"                (chunksDict chunkSym stackChunks)

haskell/src/GuitarHeroII/Convert.hs
@@ -29,7 +29,7 @@ import           GuitarHeroII.PartDrum
 import           GuitarHeroII.PartGuitar
 import           GuitarHeroII.Triggers
 import           Guitars
-import           Overdrive                        (removeNotelessOD)
+import           Overdrive                        (removeNotelessOD, voidEdgeOn)
 import           Reductions                       (gryboComplete)
 import qualified RockBand.Codec.Drums             as RB
 import qualified RockBand.Codec.Events            as RB
@@ -209,7 +209,7 @@ midiRB3toGH2 song target audio inputMid@(F.Song tmap mmap onyx) getAudioLength =
         let makeDiff diff fdiff = do
               od <- removeNotelessOD
                 mmap
-                [(fpart, [(show diff, void $ RB.fiveGems fdiff)])]
+                [(fpart, [(show diff, voidEdgeOn $ RB.fiveGems fdiff)])]
                 ((fpart,) <$> RB.fiveOverdrive rbg)
               return PartDifficulty
                 { partStarPower = snd <$> od

haskell/src/Overdrive.hs
@@ -13,6 +13,7 @@ module Overdrive
 , notesFromRB3
 , fixNotelessOD
 , removeNotelessOD
+, voidEdgeOn
 ) where
 
 import           Control.Arrow                    ((>>>))
@@ -272,6 +273,9 @@ fixPartialUnisons parts (Song tmap mmap rb3) = do
   od' <- removePartialUnisons parts mmap od
   return $ Song tmap mmap $ putOverdrive rb3 od'
 
+voidEdgeOn :: (NNC.C t) => RTB.T t (Edge s a) -> RTB.T t ()
+voidEdgeOn = RTB.mapMaybe $ \case EdgeOn{} -> Just (); EdgeOff{} -> Nothing
+
 notesFromRB3
   :: (NNC.C t)
   => FixedFile t
@@ -279,10 +283,10 @@ notesFromRB3
 notesFromRB3 FixedFile{..} = let
   five name part = do
     (diff, fd) <- Map.toAscList $ fiveDifficulties part
-    return (show diff ++ " " ++ name, void $ fiveGems fd)
+    return (show diff ++ " " ++ name, voidEdgeOn $ fiveGems fd)
   protar name part = do
     (diff, pgd) <- Map.toAscList $ pgDifficulties part
-    return (show diff ++ " Pro " ++ name, void $ pgNotes pgd)
+    return (show diff ++ " Pro " ++ name, voidEdgeOn $ pgNotes pgd)
   in do
     (fpart, notes) <-
       [ (FlexDrums,) $ do
@@ -291,10 +295,10 @@ notesFromRB3 FixedFile{..} = let
       , (FlexGuitar,) $ five "Guitar" fixedPartGuitar ++ protar "Guitar" fixedPartRealGuitar ++ protar "Guitar (22)" fixedPartRealGuitar22
       , (FlexBass,) $ five "Bass" fixedPartBass ++ protar "Bass" fixedPartRealBass ++ protar "Bass (22)" fixedPartRealBass22
       , (FlexKeys,) $ five "Keys" fixedPartKeys ++
-        [ ("Easy Pro Keys"  , void $ pkNotes fixedPartRealKeysE)
-        , ("Medium Pro Keys", void $ pkNotes fixedPartRealKeysM)
-        , ("Hard Pro Keys"  , void $ pkNotes fixedPartRealKeysH)
-        , ("Expert Pro Keys", void $ pkNotes fixedPartRealKeysX)
+        [ ("Easy Pro Keys"  , voidEdgeOn $ pkNotes fixedPartRealKeysE)
+        , ("Medium Pro Keys", voidEdgeOn $ pkNotes fixedPartRealKeysM)
+        , ("Hard Pro Keys"  , voidEdgeOn $ pkNotes fixedPartRealKeysH)
+        , ("Expert Pro Keys", voidEdgeOn $ pkNotes fixedPartRealKeysX)
         ]
       ]
     case filter (not . RTB.null . snd) notes of